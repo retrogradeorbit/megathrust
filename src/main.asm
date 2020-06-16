2ce2b3bd6189642dd57176119a45813f898e9f79
Crispin Wellington <retrogradeorbit@gmail.com>
2020-06-16 18:16:24 +0800

    rudimentary title

--- src/main.asm
+++ src/main.asm
@@ -38,6 +38,7 @@ start:
         sta vic_memory_setup
 
         jsr clear_screen
+        jsr title
 
         lda #$00
         sta raster_line
@@ -66,6 +67,66 @@ clr_loop:
         inx
         cpx #$fa
         bne clr_loop
+
+        rts
+
+title:
+        lda #$01
+        sta $0400+40*5+10
+        lda #$02
+        sta $0400+40*5+11
+        lda #$03
+        sta $0400+40*5+12
+        lda #$04
+        sta $0400+40*5+13
+        lda #$05
+        sta $0400+40*5+14
+        lda #$06
+        sta $0400+40*5+15
+        lda #$07
+        sta $0400+40*5+16
+        lda #$08
+        sta $0400+40*5+17
+        lda #$09
+        sta $0400+40*5+18
+
+        lda #$0a
+        sta $0400+40*6+10
+        lda #$0b
+        sta $0400+40*6+11
+        lda #$0c
+        sta $0400+40*6+12
+        lda #$0d
+        sta $0400+40*6+13
+        lda #$0e
+        sta $0400+40*6+14
+        lda #$0f
+        sta $0400+40*6+15
+        lda #$10
+        sta $0400+40*6+16
+        lda #$11
+        sta $0400+40*6+17
+        lda #$12
+        sta $0400+40*6+18
+
+        lda #$13
+        sta $0400+40*7+10
+        lda #$14
+        sta $0400+40*7+11
+        lda #$15
+        sta $0400+40*7+12
+        lda #$16
+        sta $0400+40*7+13
+        lda #$17
+        sta $0400+40*7+14
+        lda #$18
+        sta $0400+40*7+15
+        lda #$19
+        sta $0400+40*7+16
+        lda #$1a
+        sta $0400+40*7+17
+        lda #$1b
+        sta $0400+40*7+18
 
         rts
 
