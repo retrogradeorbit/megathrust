bdaaff1f42111c4cdb6a34ec40731196a4254d3c
Crispin Wellington <retrogradeorbit@gmail.com>
2020-06-17 13:38:42 +0800

    dedup character set and output block routine

--- process/src/process/core.clj
+++ process/src/process/core.clj
@@ -21,24 +21,75 @@
        (apply +))
   )
 
+(defn dedup [chars]
+  (into {}
+        (for [[k v] (group-by second chars)]
+          [k (map first v)])))
+
+(defn make-char-set [deduped]
+  (mapv first deduped))
+
+(defn char-locations
+  "map each char [x y] => char index"
+  [charset deduped]
+  (into {}
+        (apply concat
+               (for [[ch locs] deduped]
+                 (map
+                  (fn [l]
+                    [l (.indexOf charset ch)])
+                  locs)))))
+
+#_
+(char-locations
+ [(list 0 1 2) (list 2 3 4) (list 9 10 11)]
+ {(list 2 3 4) [[1 1] [2 2]]
+   (list 9 10 11) [[0 10]]}
+ )
+
 (defn -main
   "process charmap"
   [& args]
   (let [charmap (mikera/load-image "../gfx/charmap-01.png")
-        order [
-               ;; large MEGA THRUST text
-               [0 0]
-               [1 0] [2 0] [3 0] [4 0] [5 0] [6 0] [7 0] [8 0] [9 0]
-               [1 1] [2 1] [3 1] [4 1] [5 1] [6 1] [7 1] [8 1] [9 1]
-               [1 2] [2 2] [3 2] [4 2] [5 2] [6 2] [7 2] [8 2] [9 2]
+        order (into []
+                    (for [y (range 5)
+                          x (range 18)]
+                      [x y]))
+        chars (->> (for [[x y] order]
+                     [[x y] (for [yp (range 8)]
+                              (make-byte charmap [(* 8 x) (+ (* 8 y) yp)]))])
+                   (into {}))
+        ;;data (byte-array bytes)
 
-               [0 3] [1 3] [2 3] [3 3] [4 3] [5 3] [6 3] [7 3] [8 3] [9 3] [10 3]
-               [0 4] [1 4] [2 4] [3 4] [4 4] [5 4] [6 4] [7 4] [8 4] [9 4] [10 4]
-               [0 5] [1 5] [2 5] [3 5] [4 5] [5 5] [6 5] [7 5] [8 5] [9 5] [10 5]]
-        bytes (for [[x y] order
-                    yp (range 8)
-                    ]
-                (make-byte charmap [(* 8 x) (+ (* 8 y) yp)]))
-        data (byte-array bytes)
+        deduped (dedup chars)
+        charset (concat [(list 0 0 0 0 0 0 0 0)
+                         (list 0xff 0xff 0xff 0xff 0xff 0xff 0xff 0xff)
+                         ]
+                        (make-char-set deduped))
+        locations (char-locations charset deduped)
         ]
-    (io/copy data (io/file "../gfx/charmap-01.bin"))))
+    ;;(io/copy data (io/file "../gfx/charmap-01.bin"))
+
+    ;; (prn chars)
+    ;; (println)
+    ;; (prn deduped)
+    ;; (println)
+    ;; (prn charset)
+    ;; (println)
+    ;; (prn locations)
+
+    (let [charset-data (byte-array (flatten charset))]
+      (println "writing gfx/charmap-01.bin ...")
+      (io/copy charset-data (io/file "../gfx/charmap-01.bin")))
+
+    ;; width, height, chars.
+    (let [mega-charmap
+          (concat [18 5]
+                  (for [y (range 5)
+                        x (range 18)]
+                    (locations [x y])))]
+      (println "writing gfx/mega-chars.bin ...")
+      (io/copy (byte-array mega-charmap) (io/file "../gfx/mega-chars.bin"))
+
+      )
+    ))
--- src/main.asm
+++ src/main.asm
@@ -361,34 +361,36 @@ print_loop:
 
 title:
         // MEGA
-        ldx #$01
-        lda #40*title_ypos+title_xpos+1
-        sta printer+4
-        jsr printer
+//        ldx #$01
+//        lda #40*title_ypos+title_xpos+1
+//        sta printer+4
+//        jsr printer
+//
+//        lda #40*(1+title_ypos)+title_xpos+1
+//        sta printer+4
+//        jsr printer
+//
+//        lda #40*(2+title_ypos)+title_xpos+1
+//        sta printer+4
+//        jsr printer
+//
+//        // THRUST
+//        lda #$b
+//        sta printer+9      // line is two longer
+//
+//        lda #40*(3+title_ypos)+title_xpos
+//        sta printer+4
+//        jsr printer
+//
+//        lda #40*(4+title_ypos)+title_xpos
+//        sta printer+4
+//        jsr printer
+//
+//        lda #40*(5+title_ypos)+title_xpos
+//        sta printer+4
+//        jsr printer
 
-        lda #40*(1+title_ypos)+title_xpos+1
-        sta printer+4
-        jsr printer
-
-        lda #40*(2+title_ypos)+title_xpos+1
-        sta printer+4
-        jsr printer
-
-        // THRUST
-        lda #$b
-        sta printer+9      // line is two longer
-
-        lda #40*(3+title_ypos)+title_xpos
-        sta printer+4
-        jsr printer
-
-        lda #40*(4+title_ypos)+title_xpos
-        sta printer+4
-        jsr printer
-
-        lda #40*(5+title_ypos)+title_xpos
-        sta printer+4
-        jsr printer
+        jsr draw_char_block
 
         rts
 
@@ -427,6 +429,51 @@ cfl_jmp:
         jsr copy_lut_2
         rts
 
+draw_char_block:
+        ldy mega_charmap
+        dey
+        sty dcb_width+1
+        ldx #1
+        ldy mega_charmap,X
+        dey
+        sty dcb_height+1
+
+dcb_height:
+        ldy #1
+
+dcb_width:
+        ldx #1
+        lda mega_charmap+2,X
+        sta $04fc,X
+        dex
+        cpx #$00
+        bpl dcb_width+2
+
+        // add one row to all the locs
+        // add width to lda
+        lda dcb_width+3
+        clc
+        adc #$12
+        sta dcb_width+3
+        lda dcb_width+4
+        adc #$00
+        sta dcb_width+4
+
+        // add 40 to sta
+        lda dcb_width+6
+        clc
+        adc #40
+        sta dcb_width+6
+        lda dcb_width+7
+        adc #$00
+        sta dcb_width+7
+
+        dey
+        cpy #$00
+        bpl dcb_width
+
+        rts
+
 
         * = $2000 "colour LUT"
 colourbar_lut:
@@ -438,19 +485,23 @@ colourbar_lut:
         .byte 1, 13, 7, 3, 15, 5, 10, 14, 12, 8, 4, 2, 11, 9, 6
 
 colourbar_lut_1:
-        .byte 6, 6, 9, 11, 11, 2, 4, 4, 8, 12, 12, 14, 10, 10, 5, 15, 15, 3, 7, 7, 13, 1
-        .byte 1, 13, 7, 7, 3, 15, 15, 5, 10, 10, 14, 12, 12, 8, 4, 4, 2, 11, 11, 9, 6, 6
+        .byte 6, 6, 9, 11, 11, 2, 4, 4, 8, 12, 12, 14, 10, 10, 5, 15, 15, 3, 13, 7, 7, 1
+        .byte 1, 7, 7, 13, 3, 15, 15, 5, 10, 10, 14, 12, 12, 8, 4, 4, 2, 11, 11, 9, 6, 6
 
 
 
 colourbar_lut_2:
-        .byte 6, 9, 9, 11, 2, 2, 4, 8, 8, 12, 14, 14, 10, 5, 5, 15, 3, 3, 7, 13, 13, 1
-        .byte 1, 13, 13, 7, 3, 3, 15, 5, 5, 10, 14, 14, 12, 8, 8, 4, 2, 2, 11, 9, 9, 6
+        .byte 6, 9, 9, 11, 2, 2, 4, 8, 8, 12, 14, 14, 10, 5, 5, 15, 3, 3, 13, 13, 7, 1
+        .byte 1, 7, 13, 13, 3, 3, 15, 5, 5, 10, 14, 14, 12, 8, 8, 4, 2, 2, 11, 9, 9, 6
 
 
 old_colour_lut:
         .byte 6, 9, 11, 2, 4, 8, 12, 14, 10, 5, 15, 3, 7, 13, 1
         .byte 1, 13, 7, 3, 15, 5, 10, 14, 12, 8, 4, 2, 11, 9, 6
+
+mega_charmap:
+        .import binary "gfx/mega-chars.bin"
+
 
 
         *=$1000 "Music"
